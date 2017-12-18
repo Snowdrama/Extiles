3b1c8c3322ed40b560e9ac632cc07852b4124c56
John Close <John Close>
2017-12-18 14:09:37 -0500

    remove local libraries

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const createWindow = () => {
     mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, '../', 'Extiles.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true,
     }));
